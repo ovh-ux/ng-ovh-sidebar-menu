470f27ab67297fc520fcf59feef70ddc64a4a98a
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-09-20 16:37:07 +0200

    chore(deps): add ng-slide-down

--- src/ovh-angular-sidebar-menu-list/ovh-angular-sidebar-menu-list-item/ovh-angular-sidebar-menu-list-item.js
+++ src/ovh-angular-sidebar-menu-list/ovh-angular-sidebar-menu-list-item/ovh-angular-sidebar-menu-list-item.js
@@ -1,8 +1,9 @@
 import sidebarMenuListItemItemDirective from './ovh-angular-sidebar-menu-list-item.directive';
 import sidebarMenuListItemItemFactory from './ovh-angular-sidebar-menu-list-item.factory';
+import 'ng-slide-down';
 
 export default angular
-    .module("ovh-angular-sidebar-menu-list-item", [])
+    .module("ovh-angular-sidebar-menu-list-item", ['ng-slide-down'])
     .factory("SidebarMenuListItem", sidebarMenuListItemItemFactory)
     .directive("sidebarMenuListItem", sidebarMenuListItemItemDirective)
     .name;
